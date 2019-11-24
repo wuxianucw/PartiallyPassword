cd487ea1f1a21a26648e3fc8cc5fa2d35ced2cc4
wuxianucw <1840214768@qq.com>
2019-11-24 20:30:55 +0800

    完成1.0.0初版

--- Plugin.php
+++ Plugin.php
@@ -39,7 +39,46 @@ class PartiallyPassword_Plugin implements Typecho_Plugin_Interface{
      * @return void
      */
     public static function config(Typecho_Widget_Helper_Form $form){
-        //$form->addInput(new Typecho_Widget_Helper_Form_Element_Text('nothing', NULL, 'Hello World', _t('说点什么')));
+        $default=<<<TEXT
+<script src="https://cdn.jsdelivr.net/npm/jquery.cookie@1.4.1/jquery.cookie.min.js"></script>
+<script>
+//need jQuery ans jQuery.cookie
+function PP_set(id,uid){
+    let p=$("[name=pp-password-"+uid+"]").val();
+    if(!p)return false;
+    $.cookie("PartiallyPassword"+id,p);
+}
+</script>
+TEXT;
+        $tips=<<<TEXT
+将插入在文章末尾。可以使用的变量包括：
+<ul>
+<li><code>{currentPage}</code>：当前页面URL</li>
+</ul>
+TEXT;
+        $form->addInput(new Typecho_Widget_Helper_Form_Element_Textarea('common',NULL,$default,_t('页面公共HTML'),$tips));
+        $default=<<<TEXT
+<div style="border:1px solid #000;text-align:center">
+<p>请输入密码查看隐藏内容</p>
+<p>
+<input name="pp-password-{uniqueId}" type="password">
+<a href="{currentPage}" onclick="PP_set({id},{uniqueId})" style="outline:none;color:#fff;background-color:#66ccff;padding:2px 5px 2px 5px;border-radius:1px">提交</a>
+</p>
+<p>
+{additionalContent}
+</p>
+</div>
+TEXT;
+        $tips=<<<TEXT
+密码区域的HTML。可以使用的变量包括：
+<ul>
+<li><code>{id}</code>：当前页面加密区块编号</li>
+<li><code>{uniqueId}</code>：当前页面加密区块唯一编号</li>
+<li><code>{currentPage}</code>：当前页面URL</li>
+<li><code>{additionalContent}</code>：附加信息</li>
+</ul>
+TEXT;
+        $form->addInput(new Typecho_Widget_Helper_Form_Element_Textarea('placeholder',NULL,$default,_t('密码区域HTML'),$tips));
     }
     
     /**
@@ -61,7 +100,40 @@ class PartiallyPassword_Plugin implements Typecho_Plugin_Interface{
      */
     public static function render($content,Widget_Abstract_Contents $widget){
         if($widget->fields->pp_isEnabled){
-            
+            if(strpos($content,'[ppblock')!==false){
+                @$sep=$widget->fields->pp_sep;
+                @$pwds=$widget->fields->pp_passwords;
+                if($sep)$pwds=explode($sep,$pwds);
+                else $pwds=array($pwds);
+                $mod=count($pwds);
+                if(!$mod){
+                    $mod=1;
+                    $pwds=array('');
+                }
+                $content=preg_replace_callback('/'.self::get_shortcode_regex('ppblock').'/',function($matches)use($widget,$pwds,$mod){
+                    static $count=0;
+                    if($matches[1]=='['&&$matches[6]==']')return substr($matches[0],1,-1);//不解析类似 [[ppblock]] 双重括号的代码
+                    $now=$count%$mod;
+                    $count++;
+                    $attr=htmlspecialchars_decode($matches[3]);//还原转义前的参数列表
+                    $attrs=self::shortcode_parse_atts($attr);//获取短代码的参数
+                    $ex='';
+                    if(is_array($attrs)&&isset($attrs['ex']))$ex=$attrs['ex'];
+                    $inner=$matches[5];
+                    if($pwds[$now]=='')return $inner;
+                    $input=$_COOKIE['PartiallyPassword'.$now];
+                    if($input&&$input===$pwds[$now])return $inner;
+                    else{
+                        @$placeholder=Typecho_Widget::widget('Widget_Options')->plugin('PartiallyPassword')->placeholder;
+                        if(!$placeholder)$placeholder='<div><strong style="color:red">请配置密码区域HTML！</strong></div>';
+                        $placeholder=str_replace(array('{id}','{uniqueId}','{currentPage}','{additionalContent}'),array($now,$count,$widget->permalink,$ex),$placeholder);
+                        return $placeholder;
+                    }
+                },$content);
+                @$common_content=Typecho_Widget::widget('Widget_Options')->plugin('PartiallyPassword')->common;
+                if(!$common_content)$common_content='';
+                $content.=str_replace(array('{currentPage}'),array($widget->permalink),$common_content);
+            }
         }
         return $content;
     }
@@ -73,8 +145,96 @@ class PartiallyPassword_Plugin implements Typecho_Plugin_Interface{
      * @param $layout
      */
     public static function pluginFields($layout){
-        $layout->addItem(new Typecho_Widget_Helper_Form_Element_Select('pp_isEnabled',array(0=>'关闭',1=>'开启',),0,_t('是否开启文章部分加密'),'是否对这篇文章启用部分加密功能'));
+        $layout->addItem(new Typecho_Widget_Helper_Form_Element_Select('pp_isEnabled',array(0=>'关闭',1=>'开启'),0,_t('是否开启文章部分加密'),'是否对这篇文章启用部分加密功能'));
         $layout->addItem(new Typecho_Widget_Helper_Form_Element_Text('pp_passwords',NULL,'',_t('密码'),'按照文章中调用的顺序定义密码，如果需要多个密码，请在下面定义一个分隔符，然后在相邻密码间用分隔符分隔。详细例子将在下方给出。'));
         $layout->addItem(new Typecho_Widget_Helper_Form_Element_Text('pp_sep',NULL,'',_t('分隔符'),'用于分隔多个密码。例如填写<code>|</code>作为分隔符，填写<code>114514|1919|810</code>作为密码，则表示依次定义了三个密码：<code>114514</code> <code>1919</code> <code>810</code>。不填写分隔符默认只定义一个密码。'));
     }
+
+    /**
+     * 获取匹配短代码的正则表达式
+     * @param string $tagnames
+     * @return string
+     * @link https://github.com/WordPress/WordPress/blob/master/wp-includes/shortcodes.php#L254
+
+     */
+    public static function get_shortcode_regex( $tagname ) {
+        $tagregexp = preg_quote( $tagname );
+        // WARNING! Do not change this regex without changing do_shortcode_tag() and strip_shortcode_tag()
+        // Also, see shortcode_unautop() and shortcode.js.
+        // phpcs:disable Squiz.Strings.ConcatenationSpacing.PaddingFound -- don't remove regex indentation
+        return
+            '\\['                                // Opening bracket
+            . '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
+            . "($tagregexp)"                     // 2: Shortcode name
+            . '(?![\\w-])'                       // Not followed by word character or hyphen
+            . '('                                // 3: Unroll the loop: Inside the opening shortcode tag
+            .     '[^\\]\\/]*'                   // Not a closing bracket or forward slash
+            .     '(?:'
+            .         '\\/(?!\\])'               // A forward slash not followed by a closing bracket
+            .         '[^\\]\\/]*'               // Not a closing bracket or forward slash
+            .     ')*?'
+            . ')'
+            . '(?:'
+            .     '(\\/)'                        // 4: Self closing tag ...
+            .     '\\]'                          // ... and closing bracket
+            . '|'
+            .     '\\]'                          // Closing bracket
+            .     '(?:'
+            .         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
+            .             '[^\\[]*+'             // Not an opening bracket
+            .             '(?:'
+            .                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
+            .                 '[^\\[]*+'         // Not an opening bracket
+            .             ')*+'
+            .         ')'
+            .         '\\[\\/\\2\\]'             // Closing shortcode tag
+            .     ')?'
+            . ')'
+            . '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]]
+        // phpcs:enable
+    }
+
+    /**
+     * 获取短代码属性数组
+     * @param $text
+     * @return array|string
+     * @link https://github.com/WordPress/WordPress/blob/master/wp-includes/shortcodes.php#L508
+     */
+    public static function shortcode_parse_atts($text) {
+        $atts    = array();
+        $pattern = self::get_shortcode_atts_regex();
+        $text    = preg_replace( "/[\x{00a0}\x{200b}]+/u", ' ', $text );
+        if ( preg_match_all( $pattern, $text, $match, PREG_SET_ORDER ) ) {
+            foreach ( $match as $m ) {
+                if ( ! empty( $m[1] ) ) {
+                    $atts[ strtolower( $m[1] ) ] = stripcslashes( $m[2] );
+                } elseif ( ! empty( $m[3] ) ) {
+                    $atts[ strtolower( $m[3] ) ] = stripcslashes( $m[4] );
+                } elseif ( ! empty( $m[5] ) ) {
+                    $atts[ strtolower( $m[5] ) ] = stripcslashes( $m[6] );
+                } elseif ( isset( $m[7] ) && strlen( $m[7] ) ) {
+                    $atts[] = stripcslashes( $m[7] );
+                } elseif ( isset( $m[8] ) && strlen( $m[8] ) ) {
+                    $atts[] = stripcslashes( $m[8] );
+                } elseif ( isset( $m[9] ) ) {
+                    $atts[] = stripcslashes( $m[9] );
+                }
+            }
+            // Reject any unclosed HTML elements
+            foreach ( $atts as &$value ) {
+                if ( false !== strpos( $value, '<' ) ) {
+                    if ( 1 !== preg_match( '/^[^<]*+(?:<[^>]*+>[^<]*+)*+$/', $value ) ) {
+                        $value = '';
+                    }
+                }
+            }
+        } else {
+            $atts = ltrim( $text );
+        }
+        return $atts;
+    }
+
+    private static function get_shortcode_atts_regex(){return '/([\w-]+)\s*=\s*"([^"]*)"(?:\s|$)|([\w-]+)\s*=\s*\'([^\']*)\'(?:\s|$)|([\w-]+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|\'([^\']*)\'(?:\s|$)|(\S+)(?:\s|$)/';}
+
+    //private static function get_markdown_regex($tagName='?'){return '\\'.$tagName.'&gt; (.*)(\n\n)?';}
 }
